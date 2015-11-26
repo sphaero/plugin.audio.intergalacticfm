b23504790483e0b1ccb1418e67dd4fd8c89cb4ed
Arnaud Loonstra <arnaud@sphaero.org>
2015-11-26 12:02:57 +0100

    rename to Kodi updated channels list removed beautifulsoup

--- plugin.audio.intergalacticfm/addon.py
+++ plugin.audio.intergalacticfm/addon.py
@@ -2,7 +2,7 @@ import xbmcplugin,xbmcgui
 
 __addon__ = "Intergalactic FM"
 __addonid__ = "plugin.audio.intergalacticfm"
-__version__ = "0.0.1"
+__version__ = "0.0.2"
 
 def log(msg):
     print "[PLUGIN] '%s (%s)' " % (__addon__, __version__) + str(msg)
@@ -13,29 +13,28 @@ handle = int(sys.argv[1])
 query = sys.argv[2]
 
 title = [
-	"",
-	"MURDERCAPITAL FM", 
-	"INTERGALACTIC CLASSIX", 
-	"RADIO GALAXIA", 
-	"THE DREAM MACHINE", 
-	"THE GARDEN",
-	"RADIO FREE ROBOTRON"
-	]
+    "",
+    "MURDERCAPITAL FM", 
+    "INTERGALACTIC CLASSIX", 
+    "ALIEN BROADCASTERS", 
+    "THE DREAM MACHINE", 
+    "THE GARDEN",
+    ]
 description=[
-	"",
-	"Electro & Wave",
-	"Italo, Disco & Oldschool",
-	"House & Electronics",
-	"Soundtracks & Exotica",
-	"Space & Ambient",
-	"Experimental"
-	]
+    "",
+    "Electro & Wave",
+    "Italo, Disco & Oldschool",
+    "House & Electronics",
+    "Soundtracks & Exotica",
+    "Space & Ambient",
+    ]
 
-for x in range(1,7):
-	img="https://intergalacticfm.com/images/AppIFM"+ str(x) + "-100.png"
-	url="http://radio.intergalacticfm.com/" + str(x) + ".m3u"
-	li = xbmcgui.ListItem(title[x], description[x], thumbnailImage=img)
-	li.setProperty("IsPlayable","true")
-	xbmcplugin.addDirectoryItem(handle=handle,url=url,listitem=li)
+for x in range(1,6):
+    img="https://intergalacticfm.com/images/AppIFM"+ str(x) + "-100.png"
+    url="http://radio.intergalacticfm.com/%d.m3u" %x
+    li = xbmcgui.ListItem(title[x], description[x], thumbnailImage=img)
+    li.setProperty("IsPlayable","true")
+    xbmcplugin.addDirectoryItem(handle=handle,url=url,listitem=li)
 
 xbmcplugin.endOfDirectory(handle)
+
